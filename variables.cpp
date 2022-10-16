d5f98f6eb0191b81ab90e8be7a341886bf99f1d0
DennisRono <finnneron1@gmail.com>
2022-10-16 14:44:01 +0300

    quickly apartment into exchange luck

--- variables.cpp
+++ variables.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main() {
     int fifteen = 15;
+    double fivepointnine = 5.99;
+    char letter_D = 'D';
+    string hello = "Hello";
+    bool truthy = true;
     cout << fifteen << endl;
+    cout << fivepointnine << endl;
+    cout << letter_D << endl;
+    cout << hello << endl;
+    cout << truthy << endl;
     return 0;
 }
