07fb049718073bd73a41003f8ce8c36626e5a876
DennisRono <finnneron1@gmail.com>
2023-02-21 16:50:01 +0300

    nails acres combination join industrial

--- RoadCrossingAwareness/main.cpp
+++ RoadCrossingAwareness/main.cpp
@@ -18,18 +18,16 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
 
-    int width = 1920;
-    int height = 1080;
+    int width = 1220;
+    int height = 800;
     glutInitWindowSize(width, height);
     int x = 200;
     int y = 100;
     glutInitWindowPosition(x, y);
     int win = glutCreateWindow("Road Crossing Awareness");
     printf("window id: %d\n", win);
-
     GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
     glClearColor(Red, Green, Blue, Alpha);
-
     glutDisplayFunc(RenderSceneCB);
 
     glutMainLoop();
