7775abc7de20e361d1f12883943e42038a618b6f
DennisRono <finnneron1@gmail.com>
2023-02-21 17:56:00 +0300

    little customs circle have stepped

--- RoadCrossingAwareness/main.cpp
+++ RoadCrossingAwareness/main.cpp
@@ -17,10 +17,8 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
     glutInitWindowSize(1220, 800);
     glutInitWindowPosition(200, 100);
-    int win = glutCreateWindow("Road Crossing Awareness");
-    printf("window id: %d\n", win);
-    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
-    glClearColor(Red, Green, Blue, Alpha);
+    glutCreateWindow("Road Crossing Awareness");
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glutDisplayFunc(draw);
     glutMainLoop();
     return 0;
