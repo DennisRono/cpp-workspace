c69e21da7825d1e698899bff13cf9652a290c829
DennisRono <finnneron1@gmail.com>
2023-02-21 11:32:03 +0300

    list branch recent applied highest

--- sample_openGl/triangle.c
+++ sample_openGl/triangle.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
-#include <GL/glut.h>
+#include "GL/glut.h"
 #include <iostream>
+#include <math.h>
+#pragma comment(lib, "glut32.lib")
 #define WINDOW_TITLE "Project"
 using namespace std;
 
@@ -9,6 +11,7 @@ int m_iHeight = 600;
 
 
 void keyboardDown(unsigned char key, int x, int y){
+
   switch(key) {
   case 'Q':
   case 'q':
@@ -23,27 +26,32 @@ void idle(void){
 }
 
 
-void draw()
-{
+void draw(){
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.6,0.6,0.6,1);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 	glBegin(GL_TRIANGLES);
-		glColor3f(1,0,0);
-		glVertex3f(-0.5,0,-1);
-		glColor3f(0.5,0.5,0);
-		glVertex3f(0.5,0,-1);
-		glColor3f(0,0.5,0.5);
-		glVertex3f(0,0.5,-1);
-	glEnd();
+        glColor3f( 1, 0, 0 );
+        glVertex2f( -0.8, -0.8 );
+        glColor3f( 0, 1, 0 );
+        glVertex2f( 0.8, -0.8 );
+        glColor3f( 0, 0, 1 );
+        glVertex2f( 0, 0.9 );
+    glEnd();
+
+    glBegin(GL_POLYGON);
+        glVertex3f( -0.5, -0.5, -0.5);
+        glVertex3f( -0.5,  0.5, -0.5);
+        glVertex3f(  0.5,  0.5, -0.5);
+        glVertex3f(  0.5, -0.5, -0.5);
+    glEnd();
 
 	glutSwapBuffers();
 }
 
 
-void reshape(int width, int height)
-{
+void reshape(int width, int height){
   glViewport (0, 0, (GLsizei) width, (GLsizei) height);
   glMatrixMode (GL_PROJECTION);
   glLoadIdentity();
