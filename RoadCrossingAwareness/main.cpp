7e6082a9cca01f8feb41c6ff11aee0033b8e9d1b
DennisRono <finnneron1@gmail.com>
2023-02-21 16:32:01 +0300

    positive below toward pale highway

--- RoadCrossingAwareness/main.cpp
+++ RoadCrossingAwareness/main.cpp
@@ -7,3 +7,32 @@
 #include <GL/freeglut.h>
 
 using namespace std;
+
+static void RenderSceneCB(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+
+    int width = 1920;
+    int height = 1080;
+    glutInitWindowSize(width, height);
+    int x = 200;
+    int y = 100;
+    glutInitWindowPosition(x, y);
+    int win = glutCreateWindow("Tutorial 01");
+    printf("window id: %d\n", win);
+
+    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+    glClearColor(Red, Green, Blue, Alpha);
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glutMainLoop();
+
+    return 0;
+}
