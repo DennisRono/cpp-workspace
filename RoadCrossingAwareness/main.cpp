db1420d4e0f9d3153cb29a64d91761ef53a26545
DennisRono <finnneron1@gmail.com>
2023-02-21 13:16:01 +0300

    arrow cave wrong wash like

--- RoadCrossingAwareness/main.cpp
+++ RoadCrossingAwareness/main.cpp
@@ -166,42 +166,6 @@ void MouseGL( int button, int state, int x, int y )
 
 }
 
-void glhPerspectivef2(float *matrix, float fovyInDegrees, float aspectRatio,
-                      float znear, float zfar)
-{
-    float ymax, xmax;
-    float temp, temp2, temp3, temp4;
-    ymax = znear * tanf(fovyInDegrees * M_PI / 360.0);
-    //ymin = -ymax;
-    //xmin = -ymax * aspectRatio;
-    xmax = ymax * aspectRatio;
-    glhFrustumf2(matrix, -xmax, xmax, -ymax, ymax, znear, zfar);
-}
-void glhFrustumf2(float *matrix, float left, float right, float bottom, float top,
-                  float znear, float zfar)
-{
-    float temp, temp2, temp3, temp4;
-    temp = 2.0 * znear;
-    temp2 = right - left;
-    temp3 = top - bottom;
-    temp4 = zfar - znear;
-    matrix[0] = temp / temp2;
-    matrix[1] = 0.0;
-    matrix[2] = 0.0;
-    matrix[3] = 0.0;
-    matrix[4] = 0.0;
-    matrix[5] = temp / temp3;
-    matrix[6] = 0.0;
-    matrix[7] = 0.0;
-    matrix[8] = (right + left) / temp2;
-    matrix[9] = (top + bottom) / temp3;
-    matrix[10] = (-zfar - znear) / temp4;
-    matrix[11] = -1.0;
-    matrix[12] = 0.0;
-    matrix[13] = 0.0;
-    matrix[14] = (-temp * zfar) / temp4;
-    matrix[15] = 0.0;
-}
 
 void MotionGL( int x, int y )
 {
@@ -217,7 +181,7 @@ void ReshapeGL( int w, int h )
     glViewport( 0, 0, g_iWindowWidth, g_iWindowHeight );
     glMatrixMode( GL_PROJECTION );
     glLoadIdentity();
-    glhPerspectivef2( 60.0f, g_iWindowWidth / (GLdouble) g_iWindowHeight, 0.1, 100.0 );
+    //gluPerspective( 60.0f, g_iWindowWidth / (GLdouble) g_iWindowHeight, 0.1, 100.0 );
     glutPostRedisplay();
 
 }
