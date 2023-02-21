4aff87ed2957a5458d89a6cf78d077e9bfc6c2e9
DennisRono <finnneron1@gmail.com>
2023-02-21 17:28:00 +0300

    degree roll today taught market

--- RoadCrossingAwareness/main.cpp
+++ RoadCrossingAwareness/main.cpp
@@ -8,28 +8,9 @@
 
 using namespace std;
 
-GLuint VBO;
+void draw(){
 
-static void RenderSceneCB(){
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDisableVertexAttribArray(0);
-    glutSwapBuffers();
 }
-
-static void CreateVertexBuffer(){
-    Vector3f Vertices[3];
-    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);   // bottom left
-    Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);    // bottom right
-    Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f);     // top
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-}
-
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
@@ -45,9 +26,7 @@ int main(int argc, char** argv) {
     printf("window id: %d\n", win);
     GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
     glClearColor(Red, Green, Blue, Alpha);
-    CreateVertexBuffer();
-    glutDisplayFunc(RenderSceneCB);
-
+    glutDisplayFunc(draw);
     glutMainLoop();
 
     return 0;
