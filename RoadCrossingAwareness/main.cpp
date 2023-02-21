b9a38372eb53c2f7708df5342f47081f63400ea6
DennisRono <finnneron1@gmail.com>
2023-02-21 18:10:00 +0300

    program equally living lot political

--- RoadCrossingAwareness/main.cpp
+++ RoadCrossingAwareness/main.cpp
@@ -7,10 +7,21 @@
 #include <GL/freeglut.h>
 
 using namespace std;
-void reshape();
 
 void draw(){
 
+}
+
+void reshape(GLint w, GLint h) {
+  glViewport(0, 0, w, h);
+  glLoadIdentity();
+  glMatrixMode(GL_PROJECTION);
+  GLfloat aspect = GLfloat(w) / GLfloat(h);
+  if (w <= h) {
+    glOrtho(-2.5, 2.5, -2.5/aspect, 2.5/aspect, -10.0, 10.0);
+  } else {
+    glOrtho(-2.5*aspect, 2.5*aspect, -2.5, 2.5, -10.0, 10.0);
+  }
 }
 
 int main(int argc, char** argv) {
@@ -19,21 +30,9 @@ int main(int argc, char** argv) {
     glutInitWindowSize(1220, 800);
     glutInitWindowPosition(200, 100);
     glutCreateWindow("Road Crossing Awareness");
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glutReshapeFunc(reshape);
     glutDisplayFunc(draw);
     glutMainLoop();
     return 0;
-}
-
-void reshape(GLint w, GLint h) {
-  glViewport(0, 0, w, h);
-  glMatrixMode(GL_PROJECTION);
-  GLfloat aspect = GLfloat(w) / GLfloat(h);
-  glLoadIdentity();
-  if (w <= h) {
-    glOrtho(-2.5, 2.5, -2.5/aspect, 2.5/aspect, -10.0, 10.0);
-  } else {
-    glOrtho(-2.5*aspect, 2.5*aspect, -2.5, 2.5, -10.0, 10.0);
-  }
 }
