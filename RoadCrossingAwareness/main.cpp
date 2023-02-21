8b43924f4aef748e969d131b8b320e5e20055def
DennisRono <finnneron1@gmail.com>
2023-02-21 17:44:01 +0300

    surprise birds organization dust does

--- RoadCrossingAwareness/main.cpp
+++ RoadCrossingAwareness/main.cpp
@@ -15,19 +15,13 @@ void draw(){
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
-
-    int width = 1220;
-    int height = 800;
-    glutInitWindowSize(width, height);
-    int x = 200;
-    int y = 100;
-    glutInitWindowPosition(x, y);
+    glutInitWindowSize(1220, 800);
+    glutInitWindowPosition(200, 100);
     int win = glutCreateWindow("Road Crossing Awareness");
     printf("window id: %d\n", win);
     GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
     glClearColor(Red, Green, Blue, Alpha);
     glutDisplayFunc(draw);
     glutMainLoop();
-
     return 0;
 }
